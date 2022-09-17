c3da5a555711f3d2b718d25cd4930df6532db448
nerdstarcode <sthivendev@gmail.com>
2022-09-16 21:41:48 -0300

    bora que bora

---
+++ src/services/notificationConfig.ts
@@ -0,0 +1,8 @@
+import * as Notifications from 'expo-notifications';
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true
+    }),
+});
